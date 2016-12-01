661e3eb3cf0256ed9232af635be4eb82aeaa7799
Lawrence Hudson <quicklizard@googlemail.com>
2016-12-01 10:15:38 +0000

    [#7] older numpy

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+### v0.1.3
+
+* #7 Support older numpy
+
 ### v0.1.2
 
 * #5 Better handling of DLLs
--- pyzbar/__init__.py
+++ pyzbar/__init__.py
@@ -1,3 +1,3 @@
 """A ctypes-based wrapper around the zbar barcode reader."""
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
