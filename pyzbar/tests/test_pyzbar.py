6ded5986b1342f5de28e49aa7b483158b5d799ed
Lawrence Hudson <quicklizard@googlemail.com>
2021-10-23 22:05:46 +0100

    [#117] Support zbar library that does not provide orientation

--- pyzbar/tests/test_pyzbar.py
+++ pyzbar/tests/test_pyzbar.py
@@ -25,7 +25,7 @@ except ImportError:
 
 from pyzbar import wrapper
 from pyzbar.pyzbar import (
-    decode, Decoded, Rect, ZBarSymbol, EXTERNAL_DEPENDENCIES
+    decode, Decoded, Rect, ZBarSymbol, EXTERNAL_DEPENDENCIES, ORIENTATION_AVAILABLE
 )
 from pyzbar.pyzbar_error import PyZbarError
 
@@ -40,7 +40,7 @@ class TestDecode(unittest.TestCase):
             type='CODE128',
             rect=Rect(left=37, top=550, width=324, height=76),
             polygon=[(37, 551), (37, 625), (361, 626), (361, 550)],
-            orientation='UP',
+            orientation='UP' if ORIENTATION_AVAILABLE else None,
             quality=77,
         ),
         Decoded(
@@ -48,7 +48,7 @@ class TestDecode(unittest.TestCase):
             type='CODE128',
             rect=Rect(left=4, top=0, width=390, height=76),
             polygon=[(4, 1), (4, 75), (394, 76), (394, 0)],
-            orientation='UP',
+            orientation='UP' if ORIENTATION_AVAILABLE else None,
             quality=77,
         ),
     ]
@@ -59,7 +59,7 @@ class TestDecode(unittest.TestCase):
             type='CODE128',
             rect=Rect(left=4, top=0, width=390, height=75),
             polygon=[(4, 1), (4, 75), (394, 74), (394, 0)],
-            orientation="UP",
+            orientation="UP" if ORIENTATION_AVAILABLE else None,
             quality=76,
         ),
     ]
@@ -70,7 +70,7 @@ class TestDecode(unittest.TestCase):
             type='QRCODE',
             rect=Rect(left=27, top=27, width=145, height=145),
             polygon=[(27, 27), (27, 172), (172, 172), (172, 27)],
-            orientation='UP',
+            orientation='UP' if ORIENTATION_AVAILABLE else None,
             quality=1,
         ),
     ]
@@ -82,7 +82,7 @@ class TestDecode(unittest.TestCase):
             type='QRCODE',
             rect=Rect(left=173, top=10, width=205, height=205),
             polygon=[(173, 113), (276, 215), (378, 113), (276, 10)],
-            orientation='UP',
+            orientation='UP' if ORIENTATION_AVAILABLE else None,
             quality=1,
         ),
         Decoded(
@@ -90,7 +90,7 @@ class TestDecode(unittest.TestCase):
             type='QRCODE',
             rect=Rect(left=32, top=208, width=158, height=158),
             polygon=[(32, 352), (177, 366), (190, 222), (46, 208)],
-            orientation='RIGHT',
+            orientation='RIGHT' if ORIENTATION_AVAILABLE else None,
             quality=1,
         ),
     ]
